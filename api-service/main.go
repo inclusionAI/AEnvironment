f6ca5605c0b5f7350211aa583cac16124204fb9f
晏文 <liuding.ld@antgroup.com>
2026-02-13 14:26:42 +0800

    feat: unified auto clean env instances in api service cleaner manager

--- api-service/main.go
+++ api-service/main.go
@@ -165,7 +165,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("Invalid cleanup interval: %v", err)
 	}
-	cleanManager := service.NewAEnvCleanManager(scheduleClient, interval)
+	cleanManager := service.NewAEnvCleanManager(scheduleClient, interval).
+		WithMetrics(middleware.IncrementCleanupSuccess, middleware.IncrementCleanupFailure)
 	go cleanManager.Start()
 	defer cleanManager.Stop()
 
